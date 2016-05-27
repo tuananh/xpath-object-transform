b52f1eeb1b69154758b9ad42678a76e7a9069459
Tuan Anh Tran <me@tuananh.org>
2016-05-27 09:41:35 +0700

    optimize recursive function and add more example

---
+++ examples/dotw.js
@@ -0,0 +1,63 @@
+'use strict'
+
+let transform = require('../lib/xpath-object-transform'),
+    fs = require('fs')
+
+let template = {
+    requestId: '#',
+    currencyCode: '#USD',
+    hotelSummary: ['/result/hotels/hotel', {
+        provider: '#DOTW',
+        hotelId: '@hotelid',
+        expediaId: '#',
+        hotelInfo: {
+            name: '#',
+            address: '#',
+            city: '#',
+            postalCode: '#',
+            countryCode: '#',
+            hotelRating: '#',
+            latitude: '#',
+            longitude: '#',
+            shortDescription: '#',
+            remark: '#',
+            childPolicy: {},
+            images: {
+                thumb: '',
+                hotelImages: []
+            },
+            amenities: []
+        },
+        roomList: ['rooms/room', {
+            numberOfAdults: './@adults',
+            numberOfChildren: './@children',
+            roomNo: '#',
+            rooms: ['roomType', {
+                roomTypeCode: './@roomtypecode',
+                roomName: 'name',
+                roomAmenities: ['roomAmenities/amenity', {
+                    name: '.',
+                    code: '@id'
+                }],
+                benefits: [],
+                images: [],
+                rateList: ['rateBases/rateBasis', {
+                    rateCode: 'concat(@id,"-",@runno)',
+                    name: './@description',
+                    price: 'sum(dates/date/price/text())',
+                    cancellationPolicies: ['cancellationRules/rule', {
+                        from: 'fromDate',
+                        to: 'toDate',
+                        amount: 'cancelCharge/text()'
+                    }]
+                }]
+            }]
+        }]
+    }]
+}
+
+let xml = fs.readFileSync('./dotw.xml', 'utf8')
+
+var result = transform(xml, template)
+
+// console.log(JSON.stringify(result, null, 2))
