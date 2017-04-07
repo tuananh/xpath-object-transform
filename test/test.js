038fb5a4a6b360e04f48bd844b0ac0ba6810808e
Tuan Anh Tran <me@tuananh.org>
2017-04-07 21:04:50 +0700

    fix error in last commit

--- test/test.js
+++ test/test.js
@@ -9,7 +9,9 @@ describe('transform-test', () => {
         var xml = fs.readFileSync(__dirname + '/dotw.xml', 'utf-8')
         var template = require(__dirname + '/dotw-template.js')
 
+        console.time('transform')
         var result = transform(xml, template)
+        console.timeEnd('transform')
         result.should.have.property('hotelSummary').with.length(100)
         result.hotelSummary[0].should.have.property('roomList')
         result.hotelSummary[0].roomList[0].should.have.property('numberOfAdults')
